118aab4db5aba98cff3af9db7909ee6fcac88305
Pallav Agarwal <agpallav@gmail.com>
2021-09-25 17:08:36 -0400

    Initial Commit

---
+++ RickMortyClover/RickMortyCloverApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickMortyCloverApp.swift
+//  RickMortyClover
+//
+//  Created by Pallav Agarwal on 9/25/21.
+//
+
+import SwiftUI
+
+@main
+struct RickMortyCloverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
